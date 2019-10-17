a6503107e9e57df67caa5d1b7808a229570e37d9
camerongraff <camerongraff@mines.edu>
2019-10-17 16:45:25 -0600

    Got rid of canvas and made each array bar a div instead.

--- src/Visualizer.jsx
+++ src/Visualizer.jsx
@@ -1,70 +1,68 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Button from 'react-bootstrap/Button';
-import ButtonToolbar from 'react-bootstrap/Button';
 import './Visualizer.css'
 
 const ARRAY_SIZE = 40;
-
-var arr = [];
-
-for (var i=0; i<ARRAY_SIZE; i++) {
-    arr.push(Math.floor((Math.random()*500)));
-}
+const MAX_ARRAY_VAL = 500;
 
 class Visualizer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-          isSorted: false,
+          array: []
         };
         this.selectionSort = this.selectionSort.bind(this);
     }
 
     componentDidMount() {
-        const canvas = this.refs.canvas
-        const ctx = canvas.getContext("2d")
-        arr.forEach( function(value, index) {
-            ctx.fillRect(20*(index),canvas.height-value,20,value);
-        });
+        const temp = [];
+        for (var i=0; i<ARRAY_SIZE; i++) {
+            temp.push(Math.floor((Math.random()*MAX_ARRAY_VAL)));
+        }
+        this.setState({array:temp});
     }
 
-    componentDidUpdate() {
-        const canvas = this.refs.canvas;
-        const ctx = canvas.getContext("2d");
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        arr.forEach( function(value, index) {
-            ctx.fillRect(20*(index),canvas.height-value,20,value);
-        });
-    } 
-
     selectionSort() {
+        var temp = this.state.array;
+        if (i >= ARRAY_SIZE) return;
         for (var i = 0; i<ARRAY_SIZE; i++) {
             var min_id = i;  
             for (var j = i+1; j<ARRAY_SIZE; j++) { 
-                if (arr[j] < arr[min_id])  
+                if (temp[j] < temp[min_id])  
                     min_id = j; 
             }
-            arr[i] = arr.splice(min_id, 1, arr[i])[0];
+            temp[i] = temp.splice(min_id, 1, temp[i])[0];
         }
-        console.log(arr);
-        this.setState({isSorted : true});
+        this.setState({array:temp});
+        //console.log(arr);
     }
 
     render() {
         return(
-            <div className="Visualizer">
-                <ButtonToolbar>
-                    <Button onClick={this.selectionSort}> Sort </Button>
-                </ButtonToolbar>
-                <canvas ref="canvas"
-                    width={20*ARRAY_SIZE}
-                    height={500}
-                />
+            <div className="container">
+                <button onClick={this.selectionSort}> Sort </button>
+                {this.state.array.map((val, id) => (
+                    <div 
+                        className="array-bar"
+                        key={id}
+                        style={{
+                            backgroundColor: "red",
+                            height: `${val/(MAX_ARRAY_VAL/80)}vh`
+                        }}
+                    ></div>
+                ))}
             </div>
         )
     }
 
 }
+
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds){
+        break;
+      }
+    }
+  }
 
 export default Visualizer
