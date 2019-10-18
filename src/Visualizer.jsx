87746c9c49b78935532935536abbe645c121635b
camerongraff <camerongraff@mines.edu>
2019-10-17 21:41:31 -0600

    Added button styling and finished array bar animations.

--- src/Visualizer.jsx
+++ src/Visualizer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Visualizer.css'
 
-const ARRAY_SIZE = 50;
+const ARRAY_SIZE = 100;
 const MAX_ARRAY_VAL = 1000;
 
 //percent of screen that array container div uses
@@ -19,29 +19,37 @@ class Visualizer extends React.Component {
 
     componentDidMount() {
         //populate array values
-        const temp = [];
+        const arr = [];
         for (var i=0; i<ARRAY_SIZE; i++) {
-            temp.push(Math.floor((Math.random()*MAX_ARRAY_VAL)));
+            arr.push(Math.floor((Math.random()*MAX_ARRAY_VAL)));
         }
-        this.setState({array:temp});
+        this.setState({array:arr});
     }
 
     selectionSort() {
-        var temp = this.state.array;
+        var array = this.state.array;
         var arrayBars = document.getElementsByClassName("array-bar");
+        var prevMinId = 0;
         for (let i = 0; i<ARRAY_SIZE; i++) {
             setTimeout(() => {
                 var min_id = i;  
                 for (let j = i+1; j<ARRAY_SIZE; j++) { 
-                    if (temp[j] < temp[min_id])  
+                    if (array[j] < array[min_id])  
                         min_id = j; 
                 }
-                arrayBars[i].style.height = `${temp[min_id]/(MAX_ARRAY_VAL/HEIGHT_PROP)}vh`
-                arrayBars[i].style.backgroundColor = "green";
-                arrayBars[min_id].style.height = `${temp[i]/(MAX_ARRAY_VAL/HEIGHT_PROP)}vh`
-                temp[i] = temp.splice(min_id, 1, temp[i])[0];
-            }, 100*i);
-
+               
+                //recolor previous yellow bar back to red since no longer being swapped
+                if (prevMinId > i) arrayBars[prevMinId].style.backgroundColor = "red";
+                prevMinId = min_id; 
+                //color bar that is being swapped yellow
+                arrayBars[min_id].style.height = `${array[i]/(MAX_ARRAY_VAL/HEIGHT_PROP)}vh`
+                arrayBars[min_id].style.backgroundColor = "yellow";
+                //color minimum value bar blue since it will be in sorted order
+                arrayBars[i].style.height = `${array[min_id]/(MAX_ARRAY_VAL/HEIGHT_PROP)}vh`
+                arrayBars[i].style.backgroundColor = "blue";
+                //swap array values
+                array[i] = array.splice(min_id, 1, array[i])[0];
+            }, 50*i);
         }
         //this.setState({array:temp});
     }
