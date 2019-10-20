011c1f3a38ac5f56ba71ca7e83c3ee8cb924fc1b
camerongraff <camerongraff@mines.edu>
2019-10-20 16:56:14 -0600

    Quicksor animations working, but need to make left and right ides play simultaneously.

--- src/Visualizer.jsx
+++ src/Visualizer.jsx
@@ -84,7 +84,11 @@ class Visualizer extends React.Component {
     }
 
     quickSort(arr, low, high, arrayBars, delay) {
-        var animations = [];
+        var animations = []
+        
+        //TODO fix animations so left anf right play at the same time
+        var leftAnimations = [];
+        var RightAnimations = [];
         var temp = arr;
         /* //setTimeout(() => {
             if (low < high) {
@@ -100,7 +104,6 @@ class Visualizer extends React.Component {
         var top = -1;
         stack[++top] = low;
         stack[++top] = high;
-        var counter = 0;
 
         // Keep popping from stack while is not empty 
         while (top >= 0) {
@@ -122,7 +125,7 @@ class Visualizer extends React.Component {
                         /* arrayBars[i].style.height = `${arr[j]/(MAX_ARRAY_VAL/HEIGHT_PROP)}vh`;
                         arrayBars[j].style.height = `${arr[i]/(MAX_ARRAY_VAL/HEIGHT_PROP)}vh`; */
                     //}, ANIMATION_SPEED*counter);
-                    animations.push([ [i, arr[i]], [j, arr[j]] ]);
+                    animations.push([ [i, arr[i]], [j, arr[j]], 0 ]);
                     counter++;
                     arr[i] = arr.splice(j, 1, arr[i])[0];            }
             }
@@ -130,56 +133,66 @@ class Visualizer extends React.Component {
                 /* arrayBars[i+1].style.height = `${arr[high]/(MAX_ARRAY_VAL/HEIGHT_PROP)}vh`;
                 arrayBars[high].style.height = `${arr[i+1]/(MAX_ARRAY_VAL/HEIGHT_PROP)}vh`; */
             //}, ANIMATION_SPEED*counter);
-            animations.push([ [i+1, arr[i+1]], [high, arr[high]] ]);
+            animations.push([ [i+1, arr[i+1]], [high, arr[high]], 1 ]);
             arr[i+1] = arr.splice(high, 1, arr[i+1])[0];
             pivot = i+1;
 
 
             // If there are elements on left side of pivot, 
             // then push left side to stack 
-            if (pivot - 1 > low) { 
+            if (pivot - 1 >= low) { 
                 stack[++top] = low; 
                 stack[++top] = pivot - 1; 
             } 
 
             // If there are elements on right side of pivot, 
             // then push right side to stack 
-            if (pivot + 1 < high) { 
+            if (pivot + 1 <= high) { 
                 stack[++top] = pivot + 1; 
                 stack[++top] = high; 
             } 
         }
 
+        var prevBar1;
+        var prevBar2;
+        var sorted = [];
+        //animations
         for (let i=0; i<animations.length; i++) {
             timerIds.push(setTimeout(() => {
+                if (i>0) {
+                    if (!sorted.includes(prevBar1[0])) arrayBars[prevBar1[0]].style.backgroundColor = START_COLOR;
+                    if (!sorted.includes(prevBar2[0])) arrayBars[prevBar2[0]].style.backgroundColor = START_COLOR;
+                }
                 const bar1 = animations[i][0];
                 const bar2 = animations[i][1];
-                const piv = animations[i][2];
+                const isPiv = animations[i][2];
                 arrayBars[bar1[0]].style.height = `${bar2[1]/(MAX_ARRAY_VAL/HEIGHT_PROP)}vh`;
-                //arrayBars[bar1].style.backgroundColor = COMP_COLOR;
+                arrayBars[bar1[0]].style.backgroundColor = COMP_COLOR;
                 arrayBars[bar2[0]].style.height = `${bar1[1]/(MAX_ARRAY_VAL/HEIGHT_PROP)}vh`;
-                //arrayBars[bar2].style.backgroundColor = COMP_COLOR;
+                arrayBars[bar2[0]].style.backgroundColor = COMP_COLOR;
+                if (isPiv == 1) {
+                    sorted.push(bar1[0]);
+                    arrayBars[bar1[0]].style.backgroundColor = FINISH_COLOR;
+                }
+                prevBar1 = bar1;
+                prevBar2 = bar2;
             }, ANIMATION_SPEED*i));
         }
     }
 
-    quickSortPartition(arr, low, high, arrayBars, delay) {
+    quickSortPartition(arr, low, high, arrayBars) {
         var pivot = arr[high];
         var i = low - 1;
         for (var j=low; j<high; j++) {
             if (arr[j] < pivot) {
                 i++;
-                setTimeout(() => {
-                    arrayBars[i].style.height = `${arr[j]/(MAX_ARRAY_VAL/HEIGHT_PROP)}vh`;
-                    arrayBars[j].style.height = `${arr[i]/(MAX_ARRAY_VAL/HEIGHT_PROP)}vh`;
-                }, delay+(ANIMATION_SPEED*(i-low+1)));
+                arrayBars[i].style.height = `${arr[j]/(MAX_ARRAY_VAL/HEIGHT_PROP)}vh`;
+                arrayBars[j].style.height = `${arr[i]/(MAX_ARRAY_VAL/HEIGHT_PROP)}vh`;
                 arr[i] = arr.splice(j, 1, arr[i])[0];            }
         }
-        setTimeout(() => {
-            arrayBars[i+1].style.height = `${arr[high]/(MAX_ARRAY_VAL/HEIGHT_PROP)}vh`;
-            arrayBars[i+1].style.backgroundColor = FINISH_COLOR;
-            arrayBars[high].style.height = `${arr[i+1]/(MAX_ARRAY_VAL/HEIGHT_PROP)}vh`;
-        }, delay+(ANIMATION_SPEED*(i-low+2)));
+        arrayBars[i+1].style.height = `${arr[high]/(MAX_ARRAY_VAL/HEIGHT_PROP)}vh`;
+        arrayBars[i+1].style.backgroundColor = FINISH_COLOR;
+        arrayBars[high].style.height = `${arr[i+1]/(MAX_ARRAY_VAL/HEIGHT_PROP)}vh`;
         arr[i+1] = arr.splice(high, 1, arr[i+1])[0];
         return i+1;
     }
@@ -189,7 +202,7 @@ class Visualizer extends React.Component {
             <div className="container">
                 <div classname="button-container">
                     <button onClick={() => this.selectionSort()}> Selection Sort </button>
-                    <button onClick={() => this.quickSort(this.state.array, 0, ARRAY_SIZE-1, document.getElementsByClassName("array-bar"), 0)}> Quick Sort </button>
+                    <button onClick={() => this.quickSort(this.state.array, 0, ARRAY_SIZE-1, document.getElementsByClassName("array-bar"))}> Quick Sort </button>
                     <button onClick={() => this.generateArray()}> Generate New Array </button>
                 </div>
                 {this.state.array.map((val, id) => (
